b4d491c62bca0a7515ecc6c54424ca1829d77a27
Ali Hussain <ali.s1995@gmail.com>
2018-07-21 18:57:16 -0400

    Naive fibonacci

--- algo-data-struct/src/Week2/Problem1.java
+++ algo-data-struct/src/Week2/Problem1.java
@@ -1,5 +1,20 @@
 package Week2;
 
 public class Problem1 {
+	
+	public static long fibonacci(long n) {
+		if(n <= 1) return n; 
+		
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+	
+	public static void main(String[] args) {
+		
+		long result;
+		
+		result = fibonacci(10);
+		System.out.println(result);
+		
+	}
 
 }
