46ff298fadabbe1b94099854d3e3c2d1aa79a692
Ali Hussain <ali.s1995@gmail.com>
2018-07-21 18:47:01 -0400

    week 1 submission + drop of week 2

--- algo-data-struct/src/Week1.java
+++ algo-data-struct/src/Week1.java
@@ -1,11 +1,18 @@
+import java.util.*;
+import java.math.*;
 
 public class Week1 {
-	
+
 	public static void main(String[] args)
 	{
-		System.out.println("g");
-		int a = 1;
-		int b = 6;
-		System.out.println(a+b);
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		long[] arrNum = new long[n];
+		for (int i = 0 ; i < n ; i++) {
+			arrNum[i] = s.nextInt();
+		}
+		Arrays.sort(arrNum);
+		long result = arrNum[n-1]*(arrNum[n-2]);
+		System.out.println(result); 	
 	}
 }
